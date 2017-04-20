71dae4a2a5064bc1604c98a1e3ccc90c64ea82eb
fanlychie <fanlychie@yeah.net>
2017-04-20 10:39:01 +0800

    POJO

---
+++ src/main/java/org/fanlychie/model/Person.java
@@ -0,0 +1,18 @@
+package org.fanlychie.model;
+
+/**
+ * Created by fanlychie on 2017/4/20.
+ */
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
